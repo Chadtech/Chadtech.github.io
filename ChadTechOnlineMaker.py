03a7fb7d6d72326c0af91d54e9e0cb5e2239dcd1
ChadTech <chadstearns1@gmail.com>
2014-03-16 02:37:36 -0700

    Changed up some code

--- ChadTechOnlineMaker.py
+++ ChadTechOnlineMaker.py
@@ -10,14 +10,15 @@ chadTechOnline =  '''
 	</head>
 	<body>
 	<body bgcolor=#"000000">
-	<img src="chadtechonline.PNG" style= "position:absolute; top: 0px;left: 0px;"/>'
-	<img src="twobar.PNG" style= "position:absolute; top: 150px;left: 0px;"/>'
+	<img src="onebar.PNG" style= "position:absolute; top: 0px;left: 2px;"/>
+	<img src="chadtechonline.PNG" style= "position:absolute; top: 4px;left: 2px;"/>'
+	<img src="twobar.PNG" style= "position:absolute; top: 150px;left: 2px;"/>'
 
 '''
 
 os.chdir(os.getcwd()+'//images')
 
-nextXPos, nextYPos = 0,158
+nextXPos, nextYPos = 0,162
 
 for directoryNumber in range(len(os.listdir(os.getcwd()))):
 	beginin = '<img src='+'"./images/'+str(len(os.listdir(os.getcwd()))-1-directoryNumber)
@@ -26,17 +27,19 @@ for directoryNumber in range(len(os.listdir(os.getcwd()))):
 
 	for imageInDir in os.listdir(os.getcwd()):
 		if imageInDir.endswith('.PNG'):
-				chadTechOnline+=beginin+'/'+imageInDir+'" style= "position:absolute; top:'+str(nextYPos)+'px;left: 0px;"/>'
+				chadTechOnline+=beginin+'/'+imageInDir+'" style= "position:absolute; top:'+str(nextYPos)+'px;left: 2px;"/>'
 				im = Image.open(imageInDir)
 				imX,imY = im.size
 				nextYPos+=imY
-				chadTechOnline+='<img src="onebar.PNG" style= "position:absolute; top:'+str(nextYPos)+ 'px;left: 0px;"/>'
+				chadTechOnline+='<img src="onebar.PNG" style= "position:absolute; top:'+str(nextYPos)+ 'px;left: 2px;"/>'
 				nextYPos+=4
 
 	os.chdir(os.path.dirname(os.getcwd()))
 
-for yit in range(nextYPos):
-	chadTechOnline+='<img src="vbar.PNG" style= "position:absolute;  top:'+str(yit)+ 'px; left: 1002px;"/>'
+for yit in range(nextYPos-2):
+	chadTechOnline+='<img src="vbar.PNG" style= "position:absolute;  top:'+str(yit)+ 'px; left: 1004px;"/>'
+for yit in range(nextYPos-2):
+	chadTechOnline+='<img src="vbar.PNG" style= "position:absolute;  top:'+str(yit)+ 'px; left: 0px;"/>'
 
 os.chdir(os.path.dirname(os.getcwd()))
 chadTechOnline+='''	</body></html>'''
