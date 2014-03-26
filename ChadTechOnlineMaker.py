95280e25dc1c4619291c1628961a9ba82f022adc
ChadTech <chadstearns1@gmail.com>
2014-03-26 16:33:57 -0700

    Cleaned up my code

--- ChadTechOnlineMaker.py
+++ ChadTechOnlineMaker.py
@@ -2,14 +2,18 @@ import os
 import PIL
 from PIL import Image
 
+###############################################################################
+###############################################################################
+##### Generating the index page
+###############################################################################
+###############################################################################
 
 numOfPages=len(os.listdir(os.getcwd()+'//images'))
 
 chadTechOnline =  '''
-
-<html>
+	<html>
 	<head>
-		<title>Chadtech Online</title>
+	<title>Chadtech Online</title>
 	</head>
 	<body>
 	<body bgcolor=#"000000">
@@ -24,8 +28,7 @@ chadTechOnline =  '''
 	<a href="archive.html">
 	<img src="archivebutton.PNG" style= "position:absolute; top: 252px; left: 1010px;"/>
 	</a>
-
-'''
+	'''
 
 os.chdir(os.getcwd()+'//images')
 
@@ -45,21 +48,6 @@ for imageInDir in os.listdir(os.getcwd()):
 			chadTechOnline+='<img src="onebar.PNG" style= "position:absolute; top:'+str(nextYPos)+ 'px;left: 4px;"/>'
 			nextYPos+=4
 
-#for directoryNumber in range(len(os.listdir(os.getcwd()))):
-#	beginin = '<img src='+'"./images/'+str(len(os.listdir(os.getcwd()))-1-directoryNumber)
-#	os.chdir(os.getcwd()+'\\'+str(len(os.listdir(os.getcwd()))-1-directoryNumber))
-#	imX,imY=0,0
-#
-#	for imageInDir in os.listdir(os.getcwd()):
-#		if imageInDir.endswith('.PNG'):
-#			if imageInDir!='title.PNG':
-#				chadTechOnline+=beginin+'/'+imageInDir+'" style= "position:absolute; top:'+str(nextYPos)+'px;left: 4px;"/>'
-#				im = Image.open(imageInDir)
-#				imX,imY = im.size
-#				nextYPos+=imY
-#				chadTechOnline+='<img src="onebar.PNG" style= "position:absolute; top:'+str(nextYPos)+ 'px;left: 4px;"/>'
-#				nextYPos+=4
-
 os.chdir(os.path.dirname(os.getcwd()))
 
 for yit in range(nextYPos-4):
@@ -76,17 +64,17 @@ mainpage.close()
 
 ###############################################################################
 ###############################################################################
-
+##### For every entry (every numbered folder in the images folder), generate a page called  [number].html
+###############################################################################
+###############################################################################
 
 numberOfEntries = len(os.listdir(os.getcwd()+'\\images'))
 
 for directoryNumber in range(numberOfEntries):
 
 	thisEntryNumber = numberOfEntries-directoryNumber-1
-
 	thisPage = '''
-
-	<html>
+		<html>
 		<head>
 			<title>Chadtech Online</title>
 		</head>
@@ -95,39 +83,36 @@ for directoryNumber in range(numberOfEntries):
 		<p>ChadTech Online</p>
 		<img src="onebar.PNG" style= "position:absolute; top: 2px;left: 4px;"/>
 		<img src="chadtechonline.PNG" style= "position:absolute; top: 6px;left: 4px;"/>
-		<img src="twobar.PNG" style= "position:absolute; top: 152px;left: 4px;"/>'''
+		<img src="twobar.PNG" style= "position:absolute; top: 152px;left: 4px;"/>
+		'''
 
 	if thisEntryNumber!=(numberOfEntries-1):
-
 		thisPage+=	'''
 		<a href="'''+str(thisEntryNumber+1)+ '''.html">
 		<img src="nextbutton.PNG" style= "position:absolute; top: 160px; left: 1010px;"/>
-		</a> '''
-
+		</a>
+		 '''
 	else:
 		thisPage+='''
 		<img src="emptybutton.PNG" style= "position:absolute; top: 160px; left: 1010px;"/>
 		'''
 
 	if thisEntryNumber==0:
-
 		thisPage+= '''
 		<img src="emptybutton.PNG" style= "position:absolute; top: 206px; left: 1010px;"/>
 		'''
-
 	else:
-
 		thisPage+=	'''
 		<a href="'''+str(thisEntryNumber-1)+'''.html">
 		<img src="prevbutton.PNG" style= "position:absolute; top: 206px; left: 1010px;"/>
-		</a>'''
+		</a>
+		'''
 
 	thisPage+='''
 		<a href="archive.html">
 		<img src="archivebutton.PNG" style= "position:absolute; top: 252px; left: 1010px;"/>
 		</a>
-
-	'''
+		'''
 
 	nextXPos, nextYPos = 0,160
 
