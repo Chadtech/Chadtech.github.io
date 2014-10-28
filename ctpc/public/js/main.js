b19cba2952fd94cdf677664f69ebec8445d8ac24
Chadtech <chadstearns1@gmail.com>
2014-10-28 11:00:10 -0700

    Improved ctpc

--- ctpc/public/js/main.js
+++ ctpc/public/js/main.js
@@ -1,30 +1,43 @@
+
+/*
+addClass = (elementId, classNamet) ->
+  element = document.getElementById elementId
+  element.className += ' ' + classNamet
+
+removeClass = (elementId, className) ->
+  element = document.getElementById elementId
+  console.log element
+  element.className = 'title'
+
+scrollMenu = (event) ->
+  scrollTop = document.documentElement.scrollTop or document.body.scrollTop
+  if scrollTop > 50
+    addClass 'title', 'scrolled'
+  else
+    console.log 'A'
+    removeClass 'title', 'scrolled'
+
+window.onscroll = scrollMenu
+ */
+
 (function() {
-  var addClass, removeClass, scrollMenu;
+  var setClass;
 
-  addClass = function(elementId, classNamet) {
+  setClass = function(elementId, newClass) {
     var element;
     element = document.getElementById(elementId);
-    return element.className += ' ' + classNamet;
+    return element.className = newClass;
   };
 
-  removeClass = function(elementId, className) {
-    var element;
-    element = document.getElementById(elementId);
-    console.log(element);
-    return element.className = 'title';
-  };
-
-  scrollMenu = function(event) {
-    var scrollTop;
-    scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    if (scrollTop > 50) {
-      return addClass('title', 'scrolled');
+  window.onmousemove = function(event) {
+    console.log(event.clientX);
+    if (event.clientX < 200) {
+      setClass('menu', 'menu revealed');
+      return setClass('content', 'content revealed');
     } else {
-      console.log('A');
-      return removeClass('title', 'scrolled');
+      setClass('menu', 'menu');
+      return setClass('content', 'content');
     }
   };
-
-  window.onscroll = scrollMenu;
 
 }).call(this);
--- ctpc/src/js/main.coffee
+++ ctpc/src/js/main.coffee
@@ -1,3 +1,4 @@
+###
 addClass = (elementId, classNamet) ->
   element = document.getElementById elementId
   element.className += ' ' + classNamet
@@ -16,3 +17,17 @@ scrollMenu = (event) ->
     removeClass 'title', 'scrolled'
 
 window.onscroll = scrollMenu
+###
+
+setClass = (elementId, newClass ) ->
+  element = document.getElementById elementId
+  element.className = newClass
+
+window.onmousemove = (event) ->
+  console.log event.clientX
+  if event.clientX < 200
+    setClass 'menu', 'menu revealed'
+    setClass 'content', 'content revealed'
+  else
+    setClass 'menu', 'menu'
+    setClass 'content', 'content'
